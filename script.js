9cfc2b7cde0648bdb2088ca32f136907b6f4cbbf
Albert Marukyan <you@gmail.com>
2020-05-19 20:40:14 +0300

    First Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// const ent = document.querySelector(".news .card:last-child");
+// window.addEventListener("scroll", () => {
+//   console.log(ent.getBoundingClientRect().bottom)
+  
+//   if(Math.round(ent.getBoundingClientRect().bottom) <= 700){
+//   setInterval(() => {ent.style.transform = "translate(0)"}, 1000);
+//   }
+// })
